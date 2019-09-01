10aeb11807563c6869bc81c3a6cc75f39f960152
Gabe Gutierrez <gabedotgutierrez@gmail.com>
2019-09-01 22:50:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,8 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional
-
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
